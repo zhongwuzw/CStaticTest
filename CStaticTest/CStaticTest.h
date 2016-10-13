d18fa76120ad1245c9ebaab9ba03f6b11107f210
zhongwuzw <zhongwuzw@qq.com>
2016-10-13 16:35:38 +0800

    Initial Commit

---
+++ CStaticTest/CStaticTest.h
@@ -0,0 +1,13 @@
+//
+//  CStaticTest.h
+//  CStaticTest
+//
+//  Created by 钟武 on 2016/10/13.
+//  Copyright © 2016年 钟武. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CStaticTest : NSObject
+
+@end
---
+++ CStaticTest/CStaticTest.m
@@ -0,0 +1,13 @@
+//
+//  CStaticTest.m
+//  CStaticTest
+//
+//  Created by 钟武 on 2016/10/13.
+//  Copyright © 2016年 钟武. All rights reserved.
+//
+
+#import "CStaticTest.h"
+
+@implementation CStaticTest
+
+@end
